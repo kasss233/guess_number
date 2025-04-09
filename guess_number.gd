86a2022cc6ec515885598ed3468f6f63c24b3346
kasssssssssss <1592858973@qq.com>
2025-04-09 11:44:35 +0800

    1

--- guess_number.gd
+++ guess_number.gd
@@ -7,24 +7,24 @@ var max_number=100
 var min_number=0
 var num;
 func _ready() -> void:
-	num=randi_range(0,100)
+	num=randi_range(min_number,max_number)
 func _process(_delta: float) -> void:
-	label.text="你猜的是不是:"+var_to_str(num)
+	label.text="你想的是不是:"+var_to_str(num)
 	
 
 
 
-func _on_less_button_down() -> void:
-	min_number=num
-	num=randi_range(min_number,max_number)
+func _on_less_button_up() -> void:
+	min_number=num+1
+	num=(min_number+max_number)/2
 
 	  
 
 
-func _on_more_button_down() -> void:
-	max_number=num
-	num=randi_range(min_number,max_number)
+func _on_more_button_up() -> void:
+	max_number=num-1
+	num=(min_number+max_number)/2
 
 
-func _on_right_button_down() -> void:
+func _on_right_button_up() -> void:
 	get_tree().change_scene_to_file("res://right.tscn")
